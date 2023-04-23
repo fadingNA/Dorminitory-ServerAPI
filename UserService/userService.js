4a0fec28a6f7f5013232f36812d127aaa198659d
fadingNA <94852295+fadingNA@users.noreply.github.com>
2023-04-23 16:08:29 -0400

    Favour

--- UserService/userService.js
+++ UserService/userService.js
@@ -7,12 +7,8 @@ let Schema = mongoose.Schema;
 
 let userSchema = new Schema({
     userName: {
-        type: String,
-        unique: true
-    },
-    passWord: String,
-    favourites: [String],
-    history: [String],
+        type: String, unique: true
+    }, passWord: String, favourites: [String], history: [String],
 });
 let User;
 
@@ -28,3 +24,45 @@ module.exports.connect = function () {
         });
     });
 };
+
+modeule.exports.registerUser = function (userData) {
+    return new Promise(function (resolve, reject) {
+        if (userData.passWord !== userData.passWord2) {
+            reject("Password do not match");
+        } else {
+            bcrypt.hash(userData.password, 10).then(hash => {
+                userData.passWrod = hash;
+                let newUser = new User(userData);
+                newUser.save().then(() => {
+                    resolve("user " + userData.userName);
+                }).catch(err => {
+                    if (err.code === 11000) {
+                        reject("User Name already taken");
+                    } else {
+                        reject("There was an error creating user");
+                    }
+                })
+            }).catch(err => (reject(err)))
+        }
+    });
+};
+
+module.exports.getFavourites = function (id) {
+    return new Promise(function (resolve, reject) {
+        user.findById(id).exec().then(user => {
+            resolve(user.favourites);
+        }).catch(err => {
+            reject(`Unable to get favourite for user`)
+        })
+    })
+}
+
+module.exports.addFavourite = function (id, favId) {
+    return new Promise(function (resolve, reject) {
+        User.findById(id).exec().then(user => {
+            if (user.favourites.length < 50) {
+                User.findByIdAndUpdate()
+            }
+        })
+    })
+}
